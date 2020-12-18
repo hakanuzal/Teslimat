3a704dbde4f40fc65c6b7c61d37b92f298317c66
hakanuzal <48126270+hakanuzal@users.noreply.github.com>
2020-12-18 09:26:04 +0300

    deneme

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+#from api_constants import mongodb_password
+
+app = Flask(__name__)
+
+database_name = "API"
+DB_URI = "mongodb+srv://hakan:{}@cluster0.ink0b.mongodb.net/{}?retryWrites=true&w=majority".format(mongodb_password, database_name)
+app.config["MONGODB_HOST"] = DB_URI
+
+db = MongoEngine()
+db.init.app(app)
+
+class Book(db.Document):
+    book_id = db.IntField()
+    name = db.StringField()
+    author = db.StringField
+
+    def to_json(self):
+
+        return {
+            "book_id": self.book_id,
+            "name": self.name,
+            "author": self.author
+        }
+
+if __name__ == '__main__':
+    app.run()
+
